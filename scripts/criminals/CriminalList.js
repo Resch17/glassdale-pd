a15dbaedb516713652864cf4423267153c9472c0
Aaron Resch <aaronmresch@gmail.com>
2020-12-01 14:30:32 -0600

    added filter by crime

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,14 +1,32 @@
 import { Criminal } from "./Criminal.js";
 import { useCriminals, getCriminals } from "./CriminalProvider.js";
 
+const eventHub = document.querySelector(".container");
+const criminalTarget = document.querySelector(".criminalsContainer");
+
+eventHub.addEventListener("crimeChosen", (event) => {
+  if (event.detail.crimeThatWasChosen !== "0") {
+    const crime = event.detail.crimeThatWasChosen;
+    const matchingCriminals = criminals.filter(
+      person => person.conviction === crime
+    );
+    render(matchingCriminals);
+  }
+});
+
+let criminals = [];
+
+const render = (criminalCollection) => {
+  criminalTarget.innerHTML = "";
+  criminalCollection.forEach((criminal) => {
+    criminalTarget.innerHTML += Criminal(criminal);
+  });
+};
+
 export const criminalList = () => {
   getCriminals().then(() => {
-    const criminalTarget = document.querySelector(".criminalsContainer");
     const criminalArray = useCriminals();
-
-    for (let criminal of criminalArray) {
-      let criminalHTML = Criminal(criminal);
-      criminalTarget.innerHTML += criminalHTML;
-    }
+    criminals = criminalArray.slice();
+    render(criminalArray);
   });
 };
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { criminalList } from "./criminals/CriminalList.js";
+import { convictionSelect } from "./convictions/ConvictionSelect.js";
 
 criminalList();
+convictionSelect();
 
 
