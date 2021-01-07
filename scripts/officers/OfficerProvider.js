b7fb0e79af3204e7bbfc847c2f2bff7efd2385f7
Aaron Resch <aaronmresch@gmail.com>
2021-01-07 11:52:24 -0600

    refactored 'use' functions in all data providers for brevity

--- scripts/officers/OfficerProvider.js
+++ scripts/officers/OfficerProvider.js
@@ -1,8 +1,6 @@
 let officers = [];
 
-export const useOfficers = () => {
-  return officers.slice();
-};
+export const useOfficers = () => officers.slice();
 
 export const getOfficers = () => {
   return fetch('https://criminals.glassdale.us/officers')
